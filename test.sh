309ab1d3ef5660e0c5d5aea5c21f68b11d38e900
lucasbrito3001 <lucas.brito3001@gmail.com>
2024-08-19 11:16:41 -0300

    ci: Remove double quotes from secret name using -r flag in jq

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@
 secrets_list=($(yq -o=json '.secrets.list' helm-values.yaml | jq -rc '.[]'))
 
 for secret in ${secrets_list[@]}; do
-  secret_name="$(echo $secret | jq '.name')"
+  secret_name="$(echo $secret | jq -r '.name')"
   echo $secret_name
 done
